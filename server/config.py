d331ac63762d2e2667580aff6b201baf51be0a19
David Neudorfer <david@davidneudorfer.com>
2017-05-03 16:44:44 -0700

    hardcode tm3 uat env

--- server/config.py
+++ server/config.py
@@ -22,7 +22,7 @@ class EnvironmentConfig:
 class StagingConfig(EnvironmentConfig):
     APP_BASE_URL = os.getenv('TM_APP_BASE_URL', None)
     API_DOCS_URL = f'{APP_BASE_URL}/api-docs/swagger-ui/index.html?' + \
-        'url=http://tasking-manager-staging.eu-west-1.elasticbeanstalk.com/api/docs'
+        'url=http://uat-env.xzp6yhyrfs.us-west-2.elasticbeanstalk.com/api/docs'
     LOG_DIR = '/var/log/tasking-manager-logs'
     LOG_LEVEL = logging.DEBUG
 
