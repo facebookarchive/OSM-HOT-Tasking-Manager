ef010ae9339a95329efa3163b8e8a30d948df60b
Eric Zhang <erizha.grad2022@my.stmc.bc.ca>
2022-06-29 09:47:06 -0700

    Added date selector for street imagery
    - Date picker added to imagery page in settings
    - Database migrated with new column for the date
    
    Frontend legend visuals
    
    2 new trim toggles + image capture on legend
    
    Set date filters as default
    
    - Mapillary photo overlay now open by default in iD editor
    
    - fromDate filter set from project settings in iD editor
    
    Frontend imagery form revamp
    
    - Added image capture mode toggle
    - Settings related to image capture mode only appear if this toggle is enabled
    - Added all connected UI prompts to messages
    - "Earliest imagery capture date" shortened to "After:" positioned before date picker
    - Added mapillary organization id filter input
    
    iD/RapiD editor fromdate filter
    
    added earliestStreetImagery as a dependency, wrapped filter in if statement
    
    Backend add fields of imageCaptureMode and mapillaryOrganizationId
    
    Updates DTOs to make the new project settings persistent
    
    Mapillary API organization get call
    
    Map legend update
    
    Now updates based on whether user selects image capture mode
    
    Update editor/rapideditor
    
    Now only enables mapillary and date filter by default if image capture mode is enabled
    
    Update map.js
    
    Update action.js
    
    Fixed missing brackets
    
    Addressing comments
    
    Renamed new columns to use underscores
    Project states

--- frontend/src/components/taskSelection/action.js
+++ frontend/src/components/taskSelection/action.js
@@ -237,6 +237,8 @@ export function TaskMapAction({
                     setDisable={setDisable}
                     comment={project.changesetComment}
                     presets={project.idPresets}
+                    earliestStreetImagery={project.earliestStreetImagery}
+                    imageCaptureMode={project.imageCaptureMode}
                     imagery={formatImageryUrlCallback(project.imagery)}
                     gpxUrl={getTaskGpxUrlCallback(project.projectId, tasksIds)}
                   />
@@ -245,6 +247,8 @@ export function TaskMapAction({
                     setDisable={setDisable}
                     comment={project.changesetComment}
                     presets={project.idPresets}
+                    earliestStreetImagery={project.earliestStreetImagery}
+                    imageCaptureMode={project.imageCaptureMode}
                     imagery={formatImageryUrlCallback(project.imagery)}
                     gpxUrl={getTaskGpxUrlCallback(project.projectId, tasksIds)}
                     powerUser={project.rapidPowerUser}
--- frontend/src/components/taskSelection/index.js
+++ frontend/src/components/taskSelection/index.js
@@ -389,7 +389,7 @@ export function TaskSelection({ project, type, loading }: Object) {
               priorityAreas={priorityAreas}
               animateZoom={false}
             />
-            <TasksMapLegend />
+            <TasksMapLegend imageCaptureMode={project.imageCaptureMode} />
           </ReactPlaceholder>
         </div>
       </div>
